0feb9d7f332a99378ec7ae073cad38c9993bb1c4
AbdiGodone <abdigodone@gmail.com>
2024-04-22 18:39:32 +0100

    setters and getters equals and hashCode

--- src/main/java/Ca2GroupProject/Appointment.java
+++ src/main/java/Ca2GroupProject/Appointment.java
@@ -1,6 +1,7 @@
 package Ca2GroupProject;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 public class Appointment {
     private String patientFirstName;
@@ -19,6 +20,76 @@ public class Appointment {
         this.date = date;
         this.triageLevel = triageLevel;
         this.DoctorFullName = doctorFullName;
+    }
+
+    public String getPatientFirstName() {
+        return patientFirstName;
+    }
+
+    public void setPatientFirstName(String patientFirstName) {
+        this.patientFirstName = patientFirstName;
+    }
+
+    public String getPatientLastName() {
+        return patientLastName;
+    }
+
+    public void setPatientLastName(String patientLastName) {
+        this.patientLastName = patientLastName;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getIssue() {
+        return issue;
+    }
+
+    public void setIssue(String issue) {
+        this.issue = issue;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public int getTriageLevel() {
+        return triageLevel;
+    }
+
+
+    public void setTriageLevel(int triageLevel) {
+        this.triageLevel = triageLevel;
+    }
+
+    public String getDoctorFullName() {
+        return DoctorFullName;
+    }
+
+    public void setDoctorFullName(String doctorFullName) {
+        DoctorFullName = doctorFullName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Appointment that = (Appointment) o;
+        return triageLevel == that.triageLevel && Objects.equals(patientFirstName, that.patientFirstName) && Objects.equals(patientLastName, that.patientLastName) && Objects.equals(dateOfBirth, that.dateOfBirth) && Objects.equals(issue, that.issue) && Objects.equals(date, that.date) && Objects.equals(DoctorFullName, that.DoctorFullName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(patientFirstName, patientLastName, dateOfBirth, issue, date, triageLevel, DoctorFullName);
     }
 
 
