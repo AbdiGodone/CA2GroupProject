642b4a64afdbe2cde751f6d81d46e758d6fb89b4
AbdiGodone <abdigodone@gmail.com>
2024-04-30 22:47:12 +0100

    constructor that takes max size and initialize it

--- src/main/java/Ca2GroupProject/BoundedPriorityQueue.java
+++ src/main/java/Ca2GroupProject/BoundedPriorityQueue.java
@@ -1,4 +1,17 @@
 package Ca2GroupProject;
 
-public class BoundedPriorityQueue {
+public class BoundedPriorityQueue extends LinkedList{
+    private LinkedList queue;
+    private int maxSize;
+
+
+
+    public BoundedPriorityQueue(int maxSize){
+        if (maxSize <= 0){
+            throw new IllegalArgumentException("the maxsize cannot be less 0 or equal to 0");
+        }
+        this.maxSize = maxSize;
+    }
+
+
 }
