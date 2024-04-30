5e407522ec2fe24377b1baaf5db98a2f1e6eb9e8
AbdiGodone <abdigodone@gmail.com>
2024-04-30 22:55:50 +0100

    isFull method to get if the queue is full isEmpty method to get if the queue is empty

--- src/main/java/Ca2GroupProject/BoundedPriorityQueue.java
+++ src/main/java/Ca2GroupProject/BoundedPriorityQueue.java
@@ -13,5 +13,23 @@ public class BoundedPriorityQueue extends LinkedList{
         this.maxSize = maxSize;
     }
 
+    /**
+     * checks if the queue is full
+     * @return true if the queue is full, false otherwise
+     */
+    public boolean isFull(){
+        return super.size() == maxSize;
+    }
+
+    /**
+     * checks if the queue is empty
+     * @return true id the queue is empty, false otherwise
+     */
+    public boolean isEmpty(){
+        return super.isEmpty();
+    }
+
+
+
 
 }
